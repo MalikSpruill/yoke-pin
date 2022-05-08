8791dc28dabfe3ee8e4ad9539f2d5a99dbfb8d08
Malik <malikspruill@gmail.com>
2022-05-08 00:35:14 -0400

    Configured a mysql test to show a table on startup

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const db = require("./db/connection");
+let inquirer = require("inquirer");
+require("console.table");
 
-db.connect(err => err ? console.log(err) : console.log("Database connected!"));
+const thisFunc = async () => {
+    let params = ["Steve", "Myers", 2, 1]
+    //let change =  await db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, params);
+    let test =  await db.query("SELECT first_name, last_name, role_id, manager_id FROM employee");
+    let test2 = test[0];
+    console.log(test)
+    console.table(test2);
+}
+
+thisFunc();
+
+
+//db.connect(err => err ? console.log(err) : console.log("Database connected!"));
+
--- src/js/tracker-questions.js
+++ src/js/tracker-questions.js
@@ -1,7 +1,7 @@
 let inquirer = require("inquirer");
 
 
-let questions = (param1, param2) => {
+let trackerQuestions = (param1, param2) => {
     let initialPrompt = [
         {
             type: "list",
@@ -69,70 +69,8 @@ let questions = (param1, param2) => {
     ]
     return initialPrompt;
 }
-// let initialPrompt = [
-//     {
-//         type: "list",
-//         name: "trackerOptions",
-//         message: "What would you like to do",
-//         choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Exit"]
-//     },
-//     {
-//         type: "input",
-//         name: "addDepartment",
-//         message: "What is the name of the department",
-//         when: ({trackerOptions}) => {
-//             if (trackerOptions === "Add a department") {
-//                 return true;
-//             }
-//             return false;
-//         },
-//         validate: input => {
-//             if (input) return true;
-//             return false;
-//         }
-//     },
-//     {
-//         type: "input",
-//         name: "addRole",
-//         message: "What is the name of the role",
-//         when: ({trackerOptions}) => {
-//             if (trackerOptions === "Add a role") {
-//                 return true;
-//             }
-//             return false;
-//         },
-//         validate: input => {
-//             if (input) return true;
-//             return false;
-//         }
-//     },
-//     {
-//         type: "input",
-//         name: "addEmployee",
-//         message: "What is the name of the employee",
-//         when: ({trackerOptions}) => {
-//             if (trackerOptions === "Add an employee") {
-//                 return true;
-//             }
-//             return false;
-//         },
-//         validate: input => {
-//             if (input) return true;
-//             return false;
-//         }
-//     },
-//     {
-//         type: "list",
-//         name: "updateEmployee",
-//         message: "Which employee's role do you want to update?",
-//         when: ({trackerOptions}) => {
-//             if (trackerOptions === "Update an employee role") {
-//                 return true;
-//             }
-//             return false;
-//         },
-//         choices: [param1, param2]
-//     },
-// ]
 
-inquirer.prompt(questions(1, 2)); //test
+
+//inquirer.prompt(trackerQuestions(1, 2)); //test
+
+module.exports = trackerQuestions;
