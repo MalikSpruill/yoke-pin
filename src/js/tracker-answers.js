56f9af7fc9f3e85772cbad8878678ee8cf9a8fdc
Malik <malikspruill@gmail.com>
2022-05-08 02:26:21 -0400

    Added the addsEmployee function for handling adding an employee to the database

--- src/js/tracker-answers.js
+++ src/js/tracker-answers.js
@@ -11,7 +11,7 @@ const viewAllRoles = async () => {
     let data = await db.query("SELECT title, salary, department.name FROM role LEFT JOIN department ON role.department_id = department.id")
     let roles = data[0];
     console.table(roles);
-    process.exit();
+    process.exit(); //DELETE THIS ----------------------------------------------------------
 }
 
 const viewAllEmployees = async () => {
@@ -30,10 +30,10 @@ const addsDepartment = async () => {
     let sql = `INSERT INTO department (name) VALUES (?)`;
     let data = await db.query(sql, [addDepartment]);
     if (data) {
-        console.log("Department succesfully added!");
+        console.log("Department succesfully added to the database!");
         return;
     }
-    throw new Error("Something went wrong in department function");
+    throw new Error("Something went wrong in addsDepartment function!");
 }
 
 const addsRole = async () => {
@@ -54,13 +54,41 @@ const addsRole = async () => {
     let sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
     let data = await db.query(sql, [role.addRole, role.salary, role.department]);
     if (data) {
-        console.log("Role succesfully added!");
+        console.log("Role succesfully added to the database!");
         return;
     }
-    throw new Error("Something went wrong in department function");
+    throw new Error("Something went wrong in addsRole function!");
+}
+
+const addsEmployee = async () => {
+    let employee = await prompt([
+        {
+            name: "firstName",
+            message: "What is the employee's first name?"
+        },
+        {
+            name: "lastName",
+            message: "What is the employee's last name?"
+        },
+        {
+            name: "role",
+            message: "What is the role of the employee?"
+        },
+        {
+            name: "manager",
+            message: "What is the id of the employee's manager?"
+        },
+    ]);
+    let sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+    let data = await db.query(sql, [employee.firstName, employee.lastName, employee.role, employee.manager]);
+    if (data) {
+        console.log("Employee succesfully added to the database!");
+        return;
+    }
+    throw new Error("Something went wrong in the addsEmployee function!");
 }
 
 //addsRole();
-viewAllRoles()
+
 
 //module.exports = {viewAllDepartments, viewAllRoles, viewAllEmployees, addsDepartment, addsRole};
